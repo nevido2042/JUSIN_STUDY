530bf7fbea4c90088fffb3602a1ba8234929c5af
DESKTOP-IN7B9DF\Han <hanwoong2042@naver.com>
2024-11-17 13:08:26 +0900

    메모리 누수 확인 코드 넣기

--- 공부/길찾기 알고리즘/A스타/main.cpp
+++ 공부/길찾기 알고리즘/A스타/main.cpp
@@ -5,9 +5,24 @@
 #include "Map.h"
 #include "PathFinder.h"
 #include <time.h>
+#ifdef _DEBUG            // 메모리 누수 체크 매크로
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifndef DBG_NEW 
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) 
+#define new DBG_NEW 
+
+#endif
+#endif
 
 int main()
 {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // 메모리 누수 체크 명령어
+    //_CrtSetBreakAlloc(321); //번호로 누수 위치 확인 가능
+
     const int iMapSize(20);
 
     CMap Map(iMapSize, iMapSize);
