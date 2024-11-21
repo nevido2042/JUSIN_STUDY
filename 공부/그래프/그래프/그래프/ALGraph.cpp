780b4f2ce13c7c5caa6f157a9432768db04f4ec2
DESKTOP-IN7B9DF\Han <hanwoong2042@naver.com>
2024-11-21 17:12:47 +0900

    그래프 구현중 list를 동적할당 하는 버전

---
+++ 공부/그래프/그래프/그래프/ALGraph.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include "ALGraph.h"
+
+void CALGraph::Initialize(int _iNumV)
+{
+	m_vecADJList.reserve(_iNumV);
+	for (int i = 0; i < _iNumV; ++i)
+	{
+		m_vecADJList.push_back(new list<int>);
+	}
+	m_iNumV = _iNumV;
+	m_iNumE = 0;
+}
+
+void CALGraph::Release()
+{
+}
+
+void CALGraph::AddEdge(int _iFromV, int _iToV)
+{
+	m_vecADJList[_iFromV]->push_back(_iToV);
+	m_vecADJList[_iToV]->push_back(_iFromV);
+
+	m_vecADJList[_iFromV]->sort();
+	m_vecADJList[_iToV]->sort();
+
+	++m_iNumE;
+}
+
+void CALGraph::Show_GraphEdgeInfo()
+{
+	for (int i = 0; i < m_iNumV; ++i)
+	{
+		cout << char(i + 65) << "와 연결된 정점: ";
+		auto Iter = m_vecADJList[i]->begin();
+		while (Iter != m_vecADJList[i]->end())
+		{
+			cout << char(*Iter + 65);
+			++Iter;
+		}
+		cout << endl;
+	}
+}
---
+++ 공부/그래프/그래프/그래프/ALGraph.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <list>
+#include <vector>
+
+enum { A, B, C, D, E, F, G, H, I, J };
+
+class CALGraph
+{
+public:
+	int m_iNumV;
+	int m_iNumE;
+	vector<list<int>*> m_vecADJList;
+
+	void Initialize(int _iNumV);
+	void Release();
+	void AddEdge(int _iFromV, int _iToV);
+	void Show_GraphEdgeInfo();
+};
+
