ce8a9661af3da52f0d5019a7af77999061cff51b
DESKTOP-IN7B9DF\Han <hanwoong2042@naver.com>
2024-11-28 22:57:24 +0900

    롱타입 소숫점 짤리는거 확인

--- 숙제/3개월차/3일차-몬스터 생성, 총알 삭제/DefaultWindow/TornadoBullet.cpp
+++ 숙제/3개월차/3일차-몬스터 생성, 총알 삭제/DefaultWindow/TornadoBullet.cpp
@@ -16,7 +16,7 @@ void CTornadoBullet::Initialize()
 	m_tInfo.fCX = 10.f;
 	m_tInfo.fCY = 10.f;
 	m_fDistance = 0.f;
-	m_fSpeed = 2.f;
+	m_fSpeed = 1.5f;
 }
 
 int CTornadoBullet::Update()
@@ -27,8 +27,8 @@ int CTornadoBullet::Update()
 	m_fDistance += 0.2f;
 	m_fOrbitalAngle += 10.f;
 
-	m_tInfo.fX = m_tFocalPoint.x + m_fDistance * cosf(m_fOrbitalAngle * (PI / 180.f));
-	m_tInfo.fY = m_tFocalPoint.y - m_fDistance * sinf(m_fOrbitalAngle * (PI / 180.f));
+	//auto x = m_fSpeed * cosf(m_fAngle * (PI / 180.f));
+	//auto y = m_fSpeed * sinf(m_fAngle * (PI / 180.f));
 
 	m_tFocalPoint.x += m_fSpeed * cosf(m_fAngle * (PI / 180.f));
 	m_tFocalPoint.y -= m_fSpeed * sinf(m_fAngle * (PI / 180.f));
@@ -36,4 +36,34 @@ int CTornadoBullet::Update()
 	__super::Update_Rect();
 
 	return OBJ_NOEVENT;
+}
+
+void CTornadoBullet::Late_Update()
+{
+	if (GAME_WIN_LEFT >= m_tRect.left ||
+		GAME_WIN_RIGHT <= m_tRect.right ||
+		GAME_WIN_TOP >= m_tRect.top ||
+		GAME_WIN_BOTTOM <= m_tRect.bottom)
+	{
+		m_bDead = true;
+	}
+
+	m_tInfo.fX = m_tFocalPoint.x + m_fDistance * cosf(m_fOrbitalAngle * (PI / 180.f));
+	m_tInfo.fY = m_tFocalPoint.y - m_fDistance * sinf(m_fOrbitalAngle * (PI / 180.f));
+
+}
+
+void CTornadoBullet::Render(HDC hDC)
+{
+	/*Ellipse(hDC,
+		m_tFocalPoint.x - 1,
+		m_tFocalPoint.y - 1,
+		m_tFocalPoint.x + 1,
+		m_tFocalPoint.y + 1);*/
+
+	Ellipse(hDC,
+		m_tRect.left,
+		m_tRect.top,
+		m_tRect.right,
+		m_tRect.bottom);
 }
--- 숙제/3개월차/3일차-몬스터 생성, 총알 삭제/DefaultWindow/TornadoBullet.h
+++ 숙제/3개월차/3일차-몬스터 생성, 총알 삭제/DefaultWindow/TornadoBullet.h
@@ -9,6 +9,8 @@ public:
 public:
     void Initialize() override;
     int Update() override;
+    void Late_Update() override;
+    void Render(HDC hdc) override;
 public:
     void Set_FocalPoint(POINT _tPoint) { m_tFocalPoint = _tPoint; }
 
