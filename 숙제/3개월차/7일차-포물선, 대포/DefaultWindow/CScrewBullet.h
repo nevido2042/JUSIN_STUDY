b0d84703293f7621345155f8c16f4cc1cf3aa2f0
DESKTOP-IN7B9DF\Han <hanwoong2042@naver.com>
2024-12-04 22:09:16 +0900

    포물선을 배웠으니, 대포를 만들어보자

---
+++ 숙제/3개월차/7일차-포물선, 대포/DefaultWindow/CScrewBullet.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "CObj.h"
+
+class CScrewBullet : public CObj
+{
+public:
+	CScrewBullet();
+	virtual ~CScrewBullet();
+
+public:
+	virtual void Initialize() override;
+	virtual int Update() override;
+	virtual void Late_Update() override;
+	virtual void Render(HDC hDC) override;
+	virtual void Release() override;
+
+private:
+	POINT			m_tCenter;
+
+	float			m_fRotAngle;
+	float			m_fRotSpeed;
+
+	bool			m_bStart;
+};
+
