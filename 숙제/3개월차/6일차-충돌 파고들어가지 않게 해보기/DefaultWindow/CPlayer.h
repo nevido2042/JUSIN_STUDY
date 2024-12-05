4da40100a0b2b42985c89e65bab0934988f0099b
DESKTOP-IN7B9DF\Han <hanwoong2042@naver.com>
2024-12-05 13:26:08 +0900

    6일차-충돌 파고들어가지 않게 해보기 시작

---
+++ 숙제/3개월차/6일차-충돌 파고들어가지 않게 해보기/DefaultWindow/CPlayer.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "CObj.h"
+#include "CShield.h"
+#include "CScrewBullet.h"
+#include "CGuideBullet.h"
+
+class CPlayer :    public CObj
+{
+public:
+    CPlayer();
+    virtual ~CPlayer();
+
+public:
+    virtual void Initialize() override;
+    virtual int Update() override;
+    virtual void Late_Update() override;
+    virtual void Render(HDC hDC) override;
+    virtual void Release() override;
+
+private:
+    void        Key_Input();
+    CObj*       Create_Shield();
+
+private:
+    POINT               m_tPosin;
+
+
+
+};
+
---
+++ 숙제/3개월차/6일차-충돌 파고들어가지 않게 해보기/DefaultWindow/Define.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#define WINCX		800
+#define WINCY		600
+
+#define	PURE		= 0
+#define PI			3.141592f
+
+#define OBJ_NOEVENT			0
+#define OBJ_DEAD			1	
+
+extern HWND		g_hWnd;
+
+enum DIRECTION { DIR_LEFT, DIR_RIGHT, DIR_UP, DIR_DOWN, DIR_LU, DIR_RU, DIR_END };
+
+enum OBJID { OBJ_PLAYER, OBJ_BULLET, OBJ_MONSTER, OBJ_MOUSE, OBJ_SHIELD, OBJ_BUTTON, OBJ_END };
+
+typedef struct tagInfo
+{
+	float		fX, fY;
+	float		fCX, fCY;	// 가로, 세로 길이
+
+}INFO;
+
+
+template<typename T>
+void Safe_Delete(T& Temp)
+{
+	if (Temp)
+	{
+		delete Temp;
+		Temp = nullptr;
+	}
+}
