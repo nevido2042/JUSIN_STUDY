8ad1fb3da9fb11b8aa9d88f2c36e6cda8407ff75
DESKTOP-OG7TGCQ\한영웅 <hanwoong2042@naver.com>
2024-12-10 03:13:24 +0900

    애니메이션 해보는데 부들부들거림, 그리고 중단점 안걸리는 버그

--- 숙제/3개월차/팀 과제(2차-록맨)/무결이가빠져서섭섭해서추가해준누수를잡을뻔한케이크를먹으면서행복하게병합한채영이가 병합할뻔한 무결이껄 쉽게 결합했지만 동완이가 병합해줌 최은성_한영웅_결합/DefaultWindow/Penguin.cpp
+++ 숙제/3개월차/팀 과제(2차-록맨)/무결이가빠져서섭섭해서추가해준누수를잡을뻔한케이크를먹으면서행복하게병합한채영이가 병합할뻔한 무결이껄 쉽게 결합했지만 동완이가 병합해줌 최은성_한영웅_결합/DefaultWindow/Penguin.cpp
@@ -4,11 +4,30 @@
 #include "CBmpMgr.h"
 #include "CScrollMgr.h"
 
+CPenguin::CPenguin()
+{
+	ZeroMemory(&m_tFly1, sizeof(IMAGE));
+	ZeroMemory(&m_tFly2, sizeof(IMAGE));
+	ZeroMemory(&m_tCurImage, sizeof(IMAGE));
+}
+
+CPenguin::~CPenguin()
+{
+	Release();
+}
+
 void CPenguin::Initialize()
 {
     m_tInfo.fCX = 50.f;
     m_tInfo.fCY = 50.f;
     m_fSpeed = 3.f;
+
+	m_tFly1 = { 215,349 };
+	m_tFly2 = { 268,343 };
+
+	m_tCurImage = m_tFly1;
+
+	m_fTime = (float)GetTickCount64();
 
     CBmpMgr::Get_Instance()->Insert_Bmp(L"../Image/Rock_Man/enemy_all.bmp", L"EnemyAll");
 }
@@ -19,6 +38,7 @@ void CPenguin::Render(HDC hDC)
 	int		iScrollY = (int)CScrollMgr::Get_Instance()->Get_ScrollY();
 
 	HDC		hMemDC = CBmpMgr::Get_Instance()->Find_Image(L"EnemyAll");
+	
 
 	GdiTransparentBlt(hDC,            // 복사 받을 DC
 		m_tRect.left + iScrollX,    // 복사 받을 위치 좌표 X, Y    
@@ -26,8 +46,8 @@ void CPenguin::Render(HDC hDC)
 		(int)m_tInfo.fCX,            // 복사 받을 이미지의 가로, 세로    라이트 - 레프트 해서 길이를 넣어줘야하니까
 		(int)m_tInfo.fCY,
 		hMemDC,                        // 복사할 이미지 DC    
-		215,							// 비트맵 출력 시작 좌표(Left, top)
-		349,                           
+		m_tCurImage._iX,//215, //269, 343						// 비트맵 출력 시작 좌표(Left, top)
+		m_tCurImage._iY,//349,                           
 		(int)m_tInfo.fCX,            // 복사할 이미지의 가로, 세로
 		(int)m_tInfo.fCY,
 		RGB(128, 0, 128));
@@ -37,6 +57,18 @@ int CPenguin::Update()
 {
     if (m_bDead)
         return OBJ_DEAD;
+
+	if (m_fTime + 1000.f < (float)GetTickCount64())
+	{
+		if (m_tCurImage == m_tFly1)
+		{
+			m_tCurImage = m_tFly2;
+		}
+		else
+		{
+			m_tCurImage = m_tFly1;
+		}
+	}
 
 	int		iScrollX = (int)CScrollMgr::Get_Instance()->Get_ScrollX();
 	int		iScrollY = (int)CScrollMgr::Get_Instance()->Get_ScrollY();
