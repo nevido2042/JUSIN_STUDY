c5dbcf6ef1cfd9169eac6051abf96c989354d016
DESKTOP-OG7TGCQ\한영웅 <hanwoong2042@naver.com>
2024-12-10 21:02:39 +0900

    메인 메뉴 타이틀 추가

--- 숙제/3개월차/API 시연회/RimWorld/RimWorld/MyMenu.cpp
+++ 숙제/3개월차/API 시연회/RimWorld/RimWorld/MyMenu.cpp
@@ -16,16 +16,15 @@ CMyMenu::~CMyMenu()
 void CMyMenu::Initialize()
 {
     CBmpMgr::Get_Instance()->Insert_Bmp(L"../Image/Menu/Menu.bmp", L"Menu");
-
     CBmpMgr::Get_Instance()->Insert_Bmp(L"../Image/Button/Start.bmp", L"Start");
     CBmpMgr::Get_Instance()->Insert_Bmp(L"../Image/Button/Exit.bmp", L"Exit");
+    CBmpMgr::Get_Instance()->Insert_Bmp(L"../Image/GameTitle.bmp", L"GameTitle");
 
-
-    CObj* pButton = CAbstractFactory<CMyButton>::Create(650.f, 300.f);
+    CObj* pButton = CAbstractFactory<CMyButton>::Create(550.f, 325.f);
     pButton->Set_ImgKey(L"Start");
     CObjMgr::Get_Instance()->Add_Object(OBJ_BUTTON, pButton);
 
-    pButton = CAbstractFactory<CMyButton>::Create(650.f, 400.f);
+    pButton = CAbstractFactory<CMyButton>::Create(550.f, 400.f);
     pButton->Set_ImgKey(L"Exit");
     CObjMgr::Get_Instance()->Add_Object(OBJ_BUTTON, pButton);
 }
@@ -51,14 +50,35 @@ void CMyMenu::Render(HDC hDC)
     SelectObject(hDC, oldBrush);
     DeleteObject(myBrush);
 
+    HDC		hMemDC;
     //우주 배경
-    HDC		hMemDC = CBmpMgr::Get_Instance()->Find_Image(L"Menu");
+    hMemDC = CBmpMgr::Get_Instance()->Find_Image(L"Menu");
     BitBlt(hDC,
         0, 50, WINCX, WINCY,
         hMemDC,
         0,
         0,
         SRCCOPY);
+
+    //게임 타이틀
+    int iLeft(350), iTop(200);
+    /*hMemDC = CBmpMgr::Get_Instance()->Find_Image(L"GameTitle");
+    BitBlt(hDC,
+        iLeft, iTop, iLeft + 400, iTop + 57,
+        hMemDC,
+        0,
+        0,
+        SRCCOPY);*/
+    hMemDC = CBmpMgr::Get_Instance()->Find_Image(L"GameTitle");
+    GdiTransparentBlt(hDC,                      // 복사 받을 DC
+        iLeft, iTop,                            // 복사 받을 위치 좌표 X, Y
+        400, 57,                                // 복사 받을 이미지의 가로, 세로
+        hMemDC,                                 // 복사할 이미지 DC	
+        0,                                      // 비트맵 출력 시작 좌표(Left, top)
+        0,
+        400,                                    // 복사할 이미지의 가로, 세로
+        57,
+        RGB(255, 255, 255));                    // 제거할 색상
 
     CObjMgr::Get_Instance()->Render(hDC);
 }
