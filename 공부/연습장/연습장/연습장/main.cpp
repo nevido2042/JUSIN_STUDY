5ffd82ca01fb79501096e52bac1d15b0ba10954a
Hero <hanwoong2042@naver.com>
2025-03-08 18:43:52 +0900

    IMGUI 시작

--- 공부/연습장/연습장/연습장/main.cpp
+++ 공부/연습장/연습장/연습장/main.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cmath>
@@ -10,104 +11,7 @@ using namespace std;
 
 int main()
 {
-	/*
-	
-	뽑을 수 Numbers[i]
-	넣을 수 int iNum {1};
-
-	while(true)
-	{
-		if (스택 비었나?)
-		{
-			수 넣기 (iNum++) (+)
-		}
-		else
-		{
-		
-			if(탑이 iNum보다 크다)
-			{
-				NO
-				break;
-			}
-
-			if(iNum와 탑이 같다.)
-			{
-				POP 해라. (-)
-			}
-			else
-			{
-				수 넣기 (iNum++) (+)
-			}
-		}
-
-		if(iNum이 iN 보다 커지면)
-			break;
-	}
-	*/
-
-	int iN;
-	cin >> iN;
-
-	vector<int> Numbers(iN);
-
-	for (int i = 0; i < iN; ++i)
-	{
-		cin >> Numbers[i];
-	}
-
-	stack<int> stack;
-	list<char> PushPops;
-	
-	int iIndex{ 0 };
-	int iNum{ 1 };
-
-	while (true)
-	{
-		if (stack.empty())
-		{
-			stack.push(iNum++);
-			PushPops.push_back('+');
-		}
-		else
-		{
-			if (stack.top() > Numbers[iIndex])
-			{
-				cout << "NO";
-				return 0;
-			}
-			else if (stack.top() == Numbers[iIndex])
-			{
-				stack.pop();
-				PushPops.push_back('-');
-				++iIndex;
-			}
-			else
-			{
-				stack.push(iNum++);
-				PushPops.push_back('+');
-			}
-		}
-
-		if (iNum > iN)
-			break;
-	}
-
-	while(!stack.empty())
-	{
-		if (stack.top() == Numbers[iIndex++])
-		{
-			stack.pop();
-			PushPops.push_back('-');
-		}
-		else
-		{
-			cout << "NO";
-			return 0;
-		}
-	}
-
-	for (char c : PushPops)
-	{
-		cout << c << '\n';
-	}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 }
