79d78ef477c841d920e9dd7cfed6e7545b55a2af
Hero <hanwoong2042@naver.com>
2025-06-10 17:12:14 +0900

    스코어 만들다가 선배 만나러 감

---
+++ Course/Month7_3D/World of Tank/Client/Private/Score.cpp
@@ -0,0 +1,96 @@
+#include "Score.h"
+
+#include "GameInstance.h"
+
+CScore::CScore(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CUIObject{ pDevice, pContext }
+{
+
+}
+
+CScore::CScore(const CScore& Prototype)
+	: CUIObject(Prototype)
+{
+
+}
+
+HRESULT CScore::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CScore::Initialize(void* pArg)
+{
+	UIOBJECT_DESC* pDesc = static_cast<UIOBJECT_DESC*>(pArg);
+
+	if (FAILED(__super::Initialize(pArg)))
+		return E_FAIL;
+
+	if (FAILED(Ready_Components()))
+		return E_FAIL;
+
+
+	return S_OK;
+}
+
+void CScore::Priority_Update(_float fTimeDelta)
+{
+	if (g_bWindowResizeRequired)
+		Resize(g_iWinSizeX, g_iWinSizeY);
+}
+
+void CScore::Update(_float fTimeDelta)
+{
+
+}
+
+void CScore::Late_Update(_float fTimeDelta)
+{
+	m_pGameInstance->Add_RenderGroup(RENDERGROUP::RG_UI, this);
+}
+
+HRESULT CScore::Render()
+{
+
+
+	return S_OK;
+}
+
+HRESULT CScore::Ready_Components()
+{
+
+
+	return S_OK;
+}
+
+CScore* CScore::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	CScore* pInstance = new CScore(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX("Failed to Created : CScore");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject* CScore::Clone(void* pArg)
+{
+	CScore* pInstance = new CScore(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX("Failed to Cloned : CScore");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CScore::Free()
+{
+	__super::Free();
+
+}
---
+++ Course/Month7_3D/World of Tank/Client/Public/Score.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "UIObject.h"
+
+NS_BEGIN(Engine)
+class CShader;
+class CTexture;
+class CVIBuffer_Rect;
+NS_END
+
+NS_BEGIN(Client)
+
+class CScore final : public CUIObject
+{
+private:
+	CScore(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CScore(const CScore& Prototype);
+	virtual ~CScore() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype();
+	virtual HRESULT Initialize(void* pArg);
+	virtual void Priority_Update(_float fTimeDelta);
+	virtual void Update(_float fTimeDelta);
+	virtual void Late_Update(_float fTimeDelta);
+	virtual HRESULT Render();
+
+private:
+	HRESULT Ready_Components();
+
+public:
+	static CScore* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg) override;
+	virtual void Free() override;
+
+};
+
+NS_END
